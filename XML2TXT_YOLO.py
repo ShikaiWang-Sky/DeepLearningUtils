dbd0fe60f90662a00cbd4f95e53431c54ce04b41
Shikai Wang <seuwsk@gmail.com>
2023-09-15 13:29:59 -0500

    read width and height from XML and normalize anchor points

--- XML2TXT_YOLO.py
+++ XML2TXT_YOLO.py
@@ -2,13 +2,19 @@ import os
 import xml.etree.ElementTree as ET
 
 
-def convert_annotation(xml_file, txt_file, class_names, image_size):
+# This script converts Pascal VOC XML annotations to YOLO format
+def convert_annotation(xml_file, txt_file, class_names):
     tree = ET.parse(xml_file)
     root = tree.getroot()
-    dw = 1.0 / image_size[0]
-    dh = 1.0 / image_size[1]
 
     with open(txt_file, "w") as out_file:
+        dw, dh = 0, 0
+        for size in root.iter("size"):
+            original_width = int(size.find("width").text)
+            original_height = int(size.find("height").text)
+            dw = 1.0 / original_width
+            dh = 1.0 / original_height
+
         for obj in root.iter("object"):
             class_name = obj.find("name").text
             if class_name not in class_names:
@@ -30,9 +36,8 @@ def convert_annotation(xml_file, txt_file, class_names, image_size):
 
 # Example usage:
 class_names = ["UAV"]  # Replace with your class names
-xml_dir = "/Users/sky/code/Dataset/Drones/val/xml"
-txt_dir = "/Users/sky/code/Dataset/Drones/labels/val"
-size = (1920, 1080)  # Replace with your image size
+xml_dir = "/Users/sky/code/Dataset/Drones/train/xml"
+txt_dir = "/Users/sky/code/Dataset/Drones/labels/train"
 
 for xml_file in os.listdir(xml_dir):
     if xml_file.endswith(".xml"):
@@ -41,5 +46,4 @@ for xml_file in os.listdir(xml_dir):
             os.path.join(xml_dir, xml_file),
             os.path.join(txt_dir, txt_file),
             class_names,
-            size,
         )
